da22b8e23b058b87abed443cc3efe44b4cea48ce
Lucas <lucas.souzaduarte.73@gmail.com>
2024-01-24 12:17:40 -0300

    finalizando tarefa

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,26 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    it('Deve ter um comentário escrito "teste 1"', async () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-text'), {target: {value: 'teste 1'}})
+
+        fireEvent.click(screen.getByTestId('btn-submit'))
+        
+        await waitFor(() => {
+            expect(screen.getByTestId('comment-value')).toHaveTextContent('teste 1')
+        })
+    })
+    it('Deve ter um comentário escrito "teste 2"', async () => {
+        render(<PostComment/>);
+
+        fireEvent.change(screen.getByTestId('comment-text'), {target: {value: 'teste 2'}})
+
+        fireEvent.click(screen.getByTestId('btn-submit'))
+        
+        await waitFor(() => {
+            expect(screen.getByTestId('comment-value')).toHaveTextContent('teste 2')
+        })
+    })
 });
